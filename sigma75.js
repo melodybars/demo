7103e53d88627a9129d945bc21954e9da82f71e6
Aryan Ali <muneebhunter11@gmail.com>
2024-06-07 17:49:08 +0500

    add practice code and file

---
+++ sigma75.js
@@ -0,0 +1,11 @@
+
+
+const callback = (arg)=>{
+    console.log(arg)
+}
+const loadScript = (src,callback)=>{
+let src = document.createElement("script");
+sc.src= src;
+sc.onload=callback("Harry")
+document.append(sc)
+} 
